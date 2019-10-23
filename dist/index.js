f3a87f5132798732d33fc1e735e816d71dbd9737
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-10-23 18:23:57 +0100

    tests

--- dist/index.js
+++ dist/index.js
@@ -14,45 +14,71 @@
 
   class ByteSize {
     constructor (bytes, options) {
-      options = options || {};
-      options.units = options.units || 'metric';
-      options.precision = typeof options.precision === 'undefined' ? 1 : options.precision;
+      options = Object.assign({
+        units: 'metric',
+        precision: 1
+      }, options);
 
-      const table = [
-        { expFrom: 0, expTo: 1, metric: 'B', iec: 'B', metric_octet: 'o', iec_octet: 'o' },
-        { expFrom: 1, expTo: 2, metric: 'kB', iec: 'KiB', metric_octet: 'ko', iec_octet: 'Kio' },
-        { expFrom: 2, expTo: 3, metric: 'MB', iec: 'MiB', metric_octet: 'Mo', iec_octet: 'Mio' },
-        { expFrom: 3, expTo: 4, metric: 'GB', iec: 'GiB', metric_octet: 'Go', iec_octet: 'Gio' },
-        { expFrom: 4, expTo: 5, metric: 'TB', iec: 'TiB', metric_octet: 'To', iec_octet: 'Tio' },
-        { expFrom: 5, expTo: 6, metric: 'PB', iec: 'PiB', metric_octet: 'Po', iec_octet: 'Pio' },
-        { expFrom: 6, expTo: 7, metric: 'EB', iec: 'EiB', metric_octet: 'Eo', iec_octet: 'Eio' },
-        { expFrom: 7, expTo: 8, metric: 'ZB', iec: 'ZiB', metric_octet: 'Zo', iec_octet: 'Zio' },
-        { expFrom: 8, expTo: 9, metric: 'YB', iec: 'YiB', metric_octet: 'Yo', iec_octet: 'Yio' }
-      ];
+      const data = {
+        metric: [
+          { from: 0   , to: 1e3 , unit: 'B' },
+          { from: 1e3 , to: 1e6 , unit: 'kB' },
+          { from: 1e6 , to: 1e9 , unit: 'MB' },
+          { from: 1e9 , to: 1e12, unit: 'GB' },
+          { from: 1e12, to: 1e15, unit: 'TB' },
+          { from: 1e15, to: 1e18, unit: 'PB' },
+          { from: 1e18, to: 1e21, unit: 'EB' },
+          { from: 1e21, to: 1e24, unit: 'ZB' },
+          { from: 1e24, to: 1e27, unit: 'YB' },
+        ],
+        metric_octet: [
+          { from: 0   , to: 1e3 , unit: 'o' },
+          { from: 1e3 , to: 1e6 , unit: 'ko' },
+          { from: 1e6 , to: 1e9 , unit: 'Mo' },
+          { from: 1e9 , to: 1e12, unit: 'Go' },
+          { from: 1e12, to: 1e15, unit: 'To' },
+          { from: 1e15, to: 1e18, unit: 'Po' },
+          { from: 1e18, to: 1e21, unit: 'Eo' },
+          { from: 1e21, to: 1e24, unit: 'Zo' },
+          { from: 1e24, to: 1e27, unit: 'Yo' },
+        ],
+        iec: [
+          { from: Math.pow(1024, 0), to: Math.pow(1024, 1), unit: 'B' },
+          { from: Math.pow(1024, 1), to: Math.pow(1024, 2), unit: 'KiB' },
+          { from: Math.pow(1024, 2), to: Math.pow(1024, 3), unit: 'MiB' },
+          { from: Math.pow(1024, 3), to: Math.pow(1024, 4), unit: 'GiB' },
+          { from: Math.pow(1024, 4), to: Math.pow(1024, 5), unit: 'TiB' },
+          { from: Math.pow(1024, 5), to: Math.pow(1024, 6), unit: 'PiB' },
+          { from: Math.pow(1024, 6), to: Math.pow(1024, 7), unit: 'EiB' },
+          { from: Math.pow(1024, 7), to: Math.pow(1024, 8), unit: 'ZiB' },
+          { from: Math.pow(1024, 8), to: Math.pow(1024, 9), unit: 'YiB' },
+        ],
+        iec_octet: [
+          { from: Math.pow(1024, 0), to: Math.pow(1024, 1), unit: 'o' },
+          { from: Math.pow(1024, 1), to: Math.pow(1024, 2), unit: 'Kio' },
+          { from: Math.pow(1024, 2), to: Math.pow(1024, 3), unit: 'Mio' },
+          { from: Math.pow(1024, 3), to: Math.pow(1024, 4), unit: 'Gio' },
+          { from: Math.pow(1024, 4), to: Math.pow(1024, 5), unit: 'Tio' },
+          { from: Math.pow(1024, 5), to: Math.pow(1024, 6), unit: 'Pio' },
+          { from: Math.pow(1024, 6), to: Math.pow(1024, 7), unit: 'Eio' },
+          { from: Math.pow(1024, 7), to: Math.pow(1024, 8), unit: 'Zio' },
+          { from: Math.pow(1024, 8), to: Math.pow(1024, 9), unit: 'Yio' },
+        ],
+      };
 
-      const base = options.units === 'metric' || options.units === 'metric_octet' ? 1000 : 1024;
       const prefix = bytes < 0 ? '-' : '';
       bytes = Math.abs(bytes);
-
-      for (let i = 0; i < table.length; i++) {
-        const lower = Math.pow(base, table[i].expFrom);
-        const upper = Math.pow(base, table[i].expTo);
-        if (bytes >= lower && bytes < upper) {
-          const units = table[i][options.units];
-          if (i === 0) {
-            this.value = prefix + bytes;
-            this.unit = units;
-            return
-          } else {
-            this.value = prefix + (bytes / lower).toFixed(options.precision);
-            this.unit = units;
-            return
-          }
-        }
+      const units = data[options.units].find(u => bytes >= u.from && bytes < u.to);
+      if (units) {
+        const value = units.from === 0
+          ? prefix + bytes.toFixed(options.precision)
+          : prefix + (bytes / units.from).toFixed(options.precision);
+        this.value = value;
+        this.unit = units.unit;
+      } else {
+        this.value = prefix + bytes;
+        this.unit = '';
       }
-
-      this.value = prefix + bytes;
-      this.unit = '';
     }
 
     toString () {
--- test.js
+++ test.js
@@ -5,6 +5,7 @@ const a = require('assert')
 const tom = module.exports = new Tom('test')
 
 tom.test('metric', function () {
+  a.deepEqual(byteSize(10), { value: '10', unit: 'B' })
   a.deepEqual(byteSize(1000), { value: '1.0', unit: 'kB' })
   a.deepEqual(byteSize(-1000), { value: '-1.0', unit: 'kB' })
   a.deepEqual(byteSize(10000), { value: '10.0', unit: 'kB' })
@@ -15,21 +16,7 @@ tom.test('metric', function () {
   a.deepEqual(byteSize(234235342436356534635465), { value: '234.2', unit: 'ZB' })
   a.deepEqual(byteSize(345234235342436356534635465), { value: '345.2', unit: 'YB' })
   a.deepEqual(byteSize(3234545234235342436356534635465), { value: '3.2345452342353426e+30', unit: '' })
-})
-
-tom.test('iec', function () {
-  const options = { units: 'iec' }
-  a.deepEqual(byteSize(1000, options), { value: '1000', unit: 'B' })
-  a.deepEqual(byteSize(10000, options), { value: '9.8', unit: 'KiB' })
-  a.deepEqual(byteSize(-10000, options), { value: '-9.8', unit: 'KiB' })
-  a.deepEqual(byteSize(34565346, options), { value: '33.0', unit: 'MiB' })
-  a.deepEqual(byteSize(56356534635465, options), { value: '51.3', unit: 'TiB' })
-  a.deepEqual(byteSize(42436356534635465, options), { value: '37.7', unit: 'PiB' })
-  a.deepEqual(byteSize(5342436356534635465, options), { value: '4.6', unit: 'EiB' })
-  a.deepEqual(byteSize(234235342436356534635465, options), { value: '198.4', unit: 'ZiB' })
-  a.deepEqual(byteSize(345234235342436356534635465, options), { value: '285.6', unit: 'YiB' })
-  a.deepEqual(byteSize(3234545234235342436356534635465, options), { value: '3.2345452342353426e+30', unit: '' })
-  a.deepEqual(byteSize(9873234545234235342436356534635465, options), { value: '9.873234545234235e+33', unit: '' })
+  a.deepEqual(byteSize(-3234545234235342436356534635465), { value: '-3.2345452342353426e+30', unit: '' })
 })
 
 tom.test('metric_octet', function () {
@@ -44,11 +31,29 @@ tom.test('metric_octet', function () {
   a.deepEqual(byteSize(234235342436356534635465, options), { value: '234.2', unit: 'Zo' })
   a.deepEqual(byteSize(345234235342436356534635465, options), { value: '345.2', unit: 'Yo' })
   a.deepEqual(byteSize(3234545234235342436356534635465, options), { value: '3.2345452342353426e+30', unit: '' })
+  a.deepEqual(byteSize(-3234545234235342436356534635465, options), { value: '-3.2345452342353426e+30', unit: '' })
+})
+
+tom.test('iec', function () {
+  const options = { units: 'iec' }
+  a.deepEqual(byteSize(1000, options), { value: '1000', unit: 'B' })
+  a.deepEqual(byteSize(1024, options), { value: '1.0', unit: 'KiB' })
+  a.deepEqual(byteSize(10000, options), { value: '9.8', unit: 'KiB' })
+  a.deepEqual(byteSize(-10000, options), { value: '-9.8', unit: 'KiB' })
+  a.deepEqual(byteSize(34565346, options), { value: '33.0', unit: 'MiB' })
+  a.deepEqual(byteSize(56356534635465, options), { value: '51.3', unit: 'TiB' })
+  a.deepEqual(byteSize(42436356534635465, options), { value: '37.7', unit: 'PiB' })
+  a.deepEqual(byteSize(5342436356534635465, options), { value: '4.6', unit: 'EiB' })
+  a.deepEqual(byteSize(234235342436356534635465, options), { value: '198.4', unit: 'ZiB' })
+  a.deepEqual(byteSize(345234235342436356534635465, options), { value: '285.6', unit: 'YiB' })
+  a.deepEqual(byteSize(3234545234235342436356534635465, options), { value: '3.2345452342353426e+30', unit: '' })
+  a.deepEqual(byteSize(9873234545234235342436356534635465, options), { value: '9.873234545234235e+33', unit: '' })
 })
 
 tom.test('iec_octet', function () {
   const options = { units: 'iec_octet' }
   a.deepEqual(byteSize(1000, options), { value: '1000', unit: 'o' })
+  a.deepEqual(byteSize(1024, options), { value: '1.0', unit: 'Kio' })
   a.deepEqual(byteSize(10000, options), { value: '9.8', unit: 'Kio' })
   a.deepEqual(byteSize(34565346, options), { value: '33.0', unit: 'Mio' })
   a.deepEqual(byteSize(56356534635465, options), { value: '51.3', unit: 'Tio' })
@@ -63,7 +68,8 @@ tom.test('iec_octet', function () {
 
 tom.test('precision', function () {
   a.deepEqual(byteSize(10, { precision: 0 }), { value: '10', unit: 'B' })
-  a.deepEqual(byteSize(15, { precision: 2 }), { value: '15', unit: 'B' })
+  a.deepEqual(byteSize(10, { precision: 1 }), { value: '10.0', unit: 'B' })
+  a.deepEqual(byteSize(15, { precision: 2 }), { value: '15.00', unit: 'B' })
   a.deepEqual(byteSize(1500, { precision: 0 }), { value: '2', unit: 'kB' })
   a.deepEqual(byteSize(1500, { precision: 2 }), { value: '1.50', unit: 'kB' })
   a.deepEqual(byteSize(-1500, { precision: 2 }), { value: '-1.50', unit: 'kB' })
