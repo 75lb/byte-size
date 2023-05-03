beac2502d86e5e6795431d142bc0b4977c4c6cf0
Lloyd Brookes <lloyd.brookes@gmail.com>
2023-05-03 15:40:02 +0100

    update deps, docs, dates and CI

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.byteSize = factory());
-}(this, (function () { 'use strict';
+})(this, (function () { 'use strict';
 
   /**
    * @module byte-size
@@ -128,4 +128,4 @@
 
   return byteSize;
 
-})));
+}));
